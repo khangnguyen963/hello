c869d15d1aeef88999a81f1725c6bcbd74c64404
Khang Nguyen <knguyen20@huskers.unl.edu>
2020-08-18 17:28:52 +0000

    changed the message to my name

--- hello.c
+++ hello.c
@@ -1,12 +1,12 @@
 /**
  * Author: Khang Nguyen
  * Date:   2020
- * 
+ *
  * A simple hello world program
  */
 #include <stdlib.h>
 #include <stdio.h>
 int main(int argc, char **argv) {
-    printf ("Hello world!\n");
+    printf ("Khang Nguyen!\n");
     return 0;
 }
