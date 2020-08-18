878b4975fca30f42098fd0b7a5decd2424b281ed
Khang Nguyen <knguyen20@huskers.unl.edu>
2020-08-18 17:16:31 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+/**
+ * Author: Khang Nguyen
+ * Date:   2020
+ * 
+ * A simple hello world program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char **argv) {
+    printf ("Hello world!\n");
+    return 0;
+}
